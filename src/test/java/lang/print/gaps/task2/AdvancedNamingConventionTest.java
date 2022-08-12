849379f8b4bc73e36b47e8a1e1e1a72b3c8d8725
Abrahamyan <asatenik@mgc.mentorg.com>
2022-08-12 15:31:16 +0400

    up

--- src/test/java/lang/print/gaps/task2/AdvancedNamingConventionTest.java
+++ src/test/java/lang/print/gaps/task2/AdvancedNamingConventionTest.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AdvancedNamingConventionsTest {
+class AdvancedNamingConventionTest {
     @Test
     void namesFromJavaConvention() throws IOException {
         Path path = Paths.get("src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java");
