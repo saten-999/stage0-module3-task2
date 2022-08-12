a435fa86c77830b5e2438855aadc2d7980800f86
Abrahamyan <asatenik@mgc.mentorg.com>
2022-08-12 15:51:52 +0400

    up

---
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -0,0 +1,20 @@
+package lang.print.gaps.task2;
+
+public class AdvancedNamingConvention {
+
+    private static final int ADULT_AGE = 18;
+
+    private int age;
+    private int phoneNumber;
+
+    void callToFriend() {
+        
+    }
+
+    void callByNumber(int number) {
+        //some code
+    }
+    public static void main(String [ ] args){
+ 
+    }
+}
