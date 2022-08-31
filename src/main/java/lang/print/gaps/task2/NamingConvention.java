838cf935eb94e0452853f51ba66b228962a97cc3
Abrahamyan <asatenik@mgc.mentorg.com>
2022-08-31 11:34:10 +0400

    up

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,12 +2,12 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
+        int variable_ten= 10;
         int variable_one = 1;
         int variableTwo = 2;
         int variable_three = 3;
         final int VARIABLE_FOUR = 4;
-        final int VARIABLE_fIVE = 5;
+        final int VARIABLE_FIVE = 5;
         int variableSix = 6;
         int variableSeven = 7;
         int variableEight = 8;
